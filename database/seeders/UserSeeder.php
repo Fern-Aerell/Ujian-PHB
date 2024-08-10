28925615391afd1673ea7a9d479d242426c9324f
Fern-Aerell <fernaerell2020@gmail.com>
2024-08-10 15:48:53 +0700

    Menambah UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $userData = [
+            [
+                'name' => 'Admin',
+                'username' => 'admin',
+                'password' => Hash::make('admin#1234'),
+            ]
+        ];
+
+        foreach($userData as $user) {
+            $userModel = new User;
+            $userModel->name = $user['name'];
+            $userModel->username = $user['username'];
+            $userModel->password = $user['password'];
+            $userModel->save();
+        }
+    }
+}
