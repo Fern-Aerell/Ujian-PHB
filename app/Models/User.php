09785230d5879fddab1a3af266b9950d93df3db5
Fern-Aerell <fernaerell2020@gmail.com>
2024-09-17 08:45:01 +0700

    Menambahkan email dan email_verified_at ke migration user

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'type',
         'name',
+        'email',
+        'email_verified_at',
         'username',
         'password',
     ];
--- database/migrations/0001_01_01_000002_create_users_table.php
+++ database/migrations/0001_01_01_000002_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('type');
             $table->string('name');
             $table->string('username')->unique();
+            $table->string('email')->nullable()->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -24,7 +26,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('username')->primary();
+            $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
