af52964439764a7199252218457148dc58eae34e
Fern-Aerell <fernaerell2020@gmail.com>
2024-09-24 02:33:26 +0700

    Membuat credits text bisa di klik ke halaman github

--- resources/js/Components/Texts/CreditsText.vue
+++ resources/js/Components/Texts/CreditsText.vue
@@ -1,3 +1,3 @@
 <template>
-    <p class="text-[12px] text-center opacity-50">Made With ❤ By Fern Aerell</p>
+    <a href="https://github.com/Fern-Aerell" target="_blank" class="text-[12px] text-center opacity-50">Made With ❤ By Fern Aerell</a>
 </template>
