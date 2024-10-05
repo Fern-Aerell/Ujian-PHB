5c52f8e5da1a36a3125b843ced12b158ebdede26
Fern-Aerell <fernaerell2020@gmail.com>
2024-10-05 15:22:14 +0700

    Menambahkan fungsi utils stringFormatDateWithDay

--- resources/js/utils.ts
+++ resources/js/utils.ts
@@ -2,3 +2,8 @@ export function stringFormatDate(str: string): string
 {
     return new Date(str).toLocaleDateString('id-ID', { day: 'numeric', month: 'short', year: 'numeric' });
 }
+
+export function stringFormatDateWithDay(str: string): string
+{
+    return new Date(str).toLocaleDateString('id-ID', { weekday: 'short', day: 'numeric', month: 'short', year: 'numeric' });
+}
