2abc0a2111894c932c10f453183d81f2a12a2c56
Fern-Aerell <fernaerell2020@gmail.com>
2024-10-11 23:35:43 +0700

    Menambahkan kata sapaan kepada user di dashboard

--- resources/js/Pages/Auth/Dashboard.vue
+++ resources/js/Pages/Auth/Dashboard.vue
@@ -1,6 +1,20 @@
 <script setup lang="ts">
 import AuthLayout from '@/Layouts/AuthLayout.vue';
 import CustomHead from '@/Components/CustomHead.vue';
+import { ref, onMounted } from 'vue';
+
+const greeting = ref('');
+
+onMounted(() => {
+    const hour = new Date().getHours();
+    if (hour >= 5 && hour < 12) {
+        greeting.value = 'pagi';
+    } else if (hour >= 12 && hour < 18) {
+        greeting.value = 'siang';
+    } else {
+        greeting.value = 'malam';
+    }
+});
 </script>
 
 <template>
@@ -8,8 +22,8 @@ import CustomHead from '@/Components/CustomHead.vue';
 
     <AuthLayout title="Dashboard">
 
-        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-full">
-            <div class="p-6 text-gray-900">Anda telah berhasil masuk!</div>
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">Hai {{ $page.props.auth.user.name }}, selamat {{ greeting }} dan selamat datang!</div>
         </div>
 
     </AuthLayout>
