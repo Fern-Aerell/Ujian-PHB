99ef1cb25d3e82a86017a6c6131eece191edb325
Fern-Aerell <fernaerell2020@gmail.com>
2024-10-16 03:22:20 +0700

    Menambahkan component vue soal editor, tapi belum selesai

--- resources/js/Pages/Auth/Dev.vue
+++ resources/js/Pages/Auth/Dev.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import AuthLayout from '@/Layouts/AuthLayout.vue';
 import CustomHead from '@/Components/CustomHead.vue';
+import SoalEditor from '@/Components/SoalEditor.vue';
 </script>
 
 <template>
     <CustomHead title="Development" />
-    <AuthLayout title="Development"></AuthLayout>
+    <AuthLayout title="Development">
+        <SoalEditor/>
+    </AuthLayout>
 </template>
