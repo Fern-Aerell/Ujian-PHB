5dafb7f02d29a60268f4dbc709567596ced418b3
Fern-Aerell <fernaerell2020@gmail.com>
2024-10-18 22:45:19 +0700

    Menambahkan component svg

---
+++ resources/js/Components/Svgs/Italic.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+    width?: string;
+    height?: string;
+}>();
+</script>
+
+<template>
+    <svg :width="props.width ?? '800px'" :height="props.height ?? '800px'" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M10 3H20M4 21H14M15 3L9 21" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+</template>
