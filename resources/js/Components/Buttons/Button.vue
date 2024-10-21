7f6f327f89e988c4b755fc26f93f2d7b41d3294c
Fern-Aerell <fernaerell2020@gmail.com>
2024-10-21 19:22:37 +0700

    Memperbaiki bagaimana cara component button menerima props

--- resources/js/Components/Buttons/Button.vue
+++ resources/js/Components/Buttons/Button.vue
@@ -8,51 +8,39 @@ const props = defineProps<{
     bgColor: 'green' | 'yellow' | 'red' | 'grey' | 'danger' | 'primary' | 'white';
 }>();
 
-const textColor = ref<string>('');
-const bgColor = ref<string>('');
-
-onMounted(() => {
-
-    switch (props.textColor) {
+function getTextColor(textColor: string) {
+    switch (textColor) {
         case 'white':
-            textColor.value = '#FFFFFF';
-            break;
+            return '#FFFFFF';
         case 'black':
-            textColor.value = '#000000';
-            break;
+            return '#000000';
     }
+}
 
-    switch (props.bgColor) {
+function getBackgroundColor(backgroundColor: string) {
+    switch (backgroundColor) {
         case 'green':
-            bgColor.value = '#5BD063';
-            break;
+            return '#5BD063';
         case 'yellow':
-            bgColor.value = '#F1E07F';
-            break;
+            return '#F1E07F';
         case 'red':
-            bgColor.value = '#FFABAB';
-            break;
+            return '#FFABAB';
         case 'grey':
-            bgColor.value = '#D2D2D2';
-            break;
+            return '#D2D2D2';
         case 'danger':
-            bgColor.value = '#dc2626';
-            break;
+            return '#dc2626';
         case 'primary':
-            bgColor.value = '#1f2937';
-            break;
+            return '#1f2937';
         case 'white':
-            bgColor.value = '#ffffff';
-            break;
+            return '#ffffff';
     }
-
-});
+}
 
 </script>
 
 <template>
     <button class="font-bold py-[10px] w-full" :style="{
-        color: textColor,
-        backgroundColor: bgColor
+        color: getTextColor(props.textColor),
+        backgroundColor: getBackgroundColor(props.bgColor)
     }">{{ props.text }}</button>
 </template>
