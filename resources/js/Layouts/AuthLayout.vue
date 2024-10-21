c9f9d4cc82f4b5f9aaff1767754b189cd80084df
Fern-Aerell <fernaerell2020@gmail.com>
2024-10-21 16:58:12 +0700

    Menghilangkan implementasi magic grid, membuat navigasi di halaman config

--- resources/js/Layouts/AuthLayout.vue
+++ resources/js/Layouts/AuthLayout.vue
@@ -2,8 +2,7 @@
 import SidebarWithMenu from '@/Components/SidebarWithMenu.vue';
 import more_png from '../../assets/icons/more.webp';
 import enquire from 'enquire.js';
-import MagicGrid from 'magic-grid';
-import { h, onMounted, onUnmounted, ref } from 'vue';
+import { ref } from 'vue';
 
 const props = defineProps<{
     title: string;
@@ -11,8 +10,7 @@ const props = defineProps<{
 }>();
 
 const mobileSidebar = ref(false);
-const magicGrid = ref<MagicGrid>();
-const gridContainer = ref();
+const contentContainer = ref();
 
 function mobileSidebarToggle(value: boolean = !mobileSidebar.value) {
     mobileSidebar.value = value;
@@ -29,50 +27,6 @@ enquire.register("screen and (min-width: 1024px)", {
         mobileSidebarToggle(false);
     },
     deferSetup: true
-});
-
-let resizeObserver: ResizeObserver;
-
-onMounted(() => {
-    if(gridContainer.value && gridContainer.value.children.length > 0) {
-        magicGrid.value = new MagicGrid({
-            container: gridContainer.value,
-            animate: false,
-            items: gridContainer.value.children.length,
-            center: false,
-            gutter: 20,
-            useMin: true
-        });
-
-        magicGrid.value.listen();
-
-        resizeObserver = new ResizeObserver(() => {
-            if(magicGrid.value) {
-                magicGrid.value.positionItems();
-            }
-        });
-
-        Array.from(gridContainer.value.children).forEach(child => {
-            resizeObserver.observe(child as Element);
-        });
-
-        setTimeout(() => {
-            magicGrid.value = new MagicGrid({
-                container: gridContainer.value,
-                animate: true,
-                items: gridContainer.value.children.length,
-                center: false,
-                gutter: 20,
-                useMin: true
-            });
-        }, 200);
-    }
-});
-
-onUnmounted(() => {
-    if (gridContainer.value && gridContainer.value.children.length > 0 && resizeObserver) {
-        resizeObserver.disconnect();
-    }
 });
 
 </script>
@@ -95,11 +49,8 @@ onUnmounted(() => {
                 </div>
             </div>
             <!-- CONTENT -->
-             <div class="overflow-auto" :class="[props.class, {'w-full !h-full': gridContainer && gridContainer.children.length > 0}]" ref="gridContainer">
+            <div ref="contentContainer" class="overflow-auto" :class="props.class">
                 <slot/>
-            </div>
-            <div v-if="gridContainer && gridContainer.children.length == 0" class="flex justify-center items-center w-full h-full">
-                <h1 class="opacity-20 font-semibold">Kosong</h1>
             </div>
         </div>
     </div>
--- resources/js/Pages/Auth/Admin/Config/Config.vue
+++ resources/js/Pages/Auth/Admin/Config/Config.vue
@@ -10,6 +10,18 @@ import ConfigKelasData from './components/ConfigKelasData.vue';
 import ConfigKelasKategoriData from './components/ConfigKelasKategoriData.vue';
 import ConfigMapelData from './components/ConfigMapelData.vue';
 import { Kelas, KelasKategori, Mapel } from '@/types';
+import { ref } from 'vue';
+
+enum Menus {
+    JADWAL = 'jadwal',
+    WAKTU = 'waktu',
+    KELAS = 'kelas',
+    KELAS_KATEGORI = 'kelas_kategori',
+    MAPEL = 'mapel',
+    AKTIFITAS = 'aktifitas',
+    SLIDER = 'slider',
+    DATA_SEKOLAH = 'data_sekolah',
+}
 
 const props = defineProps<{
     kelas: Kelas[],
@@ -17,18 +29,31 @@ const props = defineProps<{
     mapels: Mapel[],
 }>();
 
+const menuSelected = ref<Menus>(Menus.JADWAL);
 </script>
 
 <template>
     <CustomHead title="Config" />
-    <AuthLayout title="Config">
-        <ConfigExamScheduleData />
-        <ConfigExamTimeData />
-        <ConfigKelasData :kelas="props.kelas" />
-        <ConfigKelasKategoriData :kelas_kategoris="props.kelas_kategoris" />
-        <ConfigMapelData :kelas="props.kelas" :kelas_kategoris="props.kelas_kategoris" :mapels="props.mapels" />
-        <ConfigActivityData />
-        <ConfigSliderData />
-        <ConfigSchoolData />
+    <AuthLayout title="Config" class="flex flex-col gap-3">
+        <div class="flex flex-row gap-3">
+            <button 
+                v-for="(menu, index) in Menus"
+                @click="menuSelected = menu" 
+                :key="index"
+                class="py-2 px-5 font-semibold flex-shrink-0"
+                :class="{
+                    'bg-slate-300 text-black': menuSelected != menu,
+                    'bg-slate-500 text-white': menuSelected == menu,
+                }"
+            >{{ menu.split('_').map((menu) => `${menu[0].toUpperCase()}${menu.substring(1, menu.length)}`).join(' ') }}</button>
+        </div>
+        <ConfigExamScheduleData v-if="menuSelected == Menus.JADWAL" />
+        <ConfigExamTimeData v-else-if="menuSelected == Menus.WAKTU" />
+        <ConfigKelasData v-else-if="menuSelected == Menus.KELAS" :kelas="props.kelas" />
+        <ConfigKelasKategoriData v-else-if="menuSelected == Menus.KELAS_KATEGORI" :kelas_kategoris="props.kelas_kategoris" />
+        <ConfigMapelData v-else-if="menuSelected == Menus.MAPEL" :kelas="props.kelas" :kelas_kategoris="props.kelas_kategoris" :mapels="props.mapels" />
+        <ConfigActivityData v-else-if="menuSelected == Menus.AKTIFITAS"/>
+        <ConfigSliderData v-else-if="menuSelected == Menus.SLIDER"/>
+        <ConfigSchoolData v-else-if="menuSelected == Menus.DATA_SEKOLAH"/>
     </AuthLayout>
 </template>
