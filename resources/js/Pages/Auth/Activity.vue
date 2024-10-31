493847ad767e82e8cd1a4fc816b9facc657f44e8
Fern-Aerell <fernaerell2020@gmail.com>
2024-11-01 04:28:18 +0700

    Update activity.vue

--- resources/js/Pages/Auth/Activity.vue
+++ resources/js/Pages/Auth/Activity.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
 import AuthLayout from '@/Layouts/AuthLayout.vue';
 import CustomHead from '@/Components/CustomHead.vue';
+import { EUserType } from '@/types/enum.d';
 </script>
 
 <template>
     <CustomHead :title="$page.props.config.activity_type" />
-    <AuthLayout :title="$page.props.config.activity_type"></AuthLayout>
+    <AuthLayout :title="$page.props.config.activity_type">
+        <!-- GURU -->
+        <template v-if="$page.props.auth.user.type == EUserType.GURU">
+            <div v-for="(data, index) in $page.props.errors"></div>
+        </template>
+    </AuthLayout>
 </template>
