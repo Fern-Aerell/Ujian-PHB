152a026f28e8e713ce9af15b656cfe5d19c7d728
Fern-Aerell <fernaerell2020@gmail.com>
2024-11-08 07:44:03 +0700

    Membuat rapor dapat memperlihatkan nilai ujian yang sudah kita kerjakan

--- app/Http/Controllers/Auth/RaporController.php
+++ app/Http/Controllers/Auth/RaporController.php
@@ -3,11 +3,78 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\DoActivity;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RaporController extends Controller
 {
-    public function index() {
-        return inertia('Auth/Rapor');
+    public function index()
+    {
+        $doActivitys = DoActivity::with([
+            'user',
+            'activity.user',
+            'activity.activityMapelKelasKategoriKelas.mapel',
+            'activity.activityMapelKelasKategoriKelas.kelasKategori',
+            'activity.activityMapelKelasKategoriKelas.kelas',
+            'activity.activitySoals.soal.mapel',
+            'activity.activitySoals.soal.kelas',
+            'activity.activitySoals.soal.kelasKategori',
+            'activity.activitySoals.soal.jawabans',
+            'doActivitySoals.soal',
+            'doActivitySoals.soal.jawabans',
+            'doActivitySoals.doActivityJawabans',
+        ])
+        ->where('finished', true)
+        ->get();
+
+        $datas = [];
+
+        foreach ($doActivitys as $doActivity) {
+            $total_soal = count($doActivity->doActivitySoals);
+            $nilai = 0; // Inisialisasi nilai
+
+            // Hitung nilai berdasarkan jawaban
+            foreach ($doActivity->doActivitySoals as $doActivitySoal) {
+                $soal = $doActivitySoal->soal;
+                $jawabanBenar = [];
+                $jumlahBenar = 0;
+                foreach($soal->jawabans as $jawaban) {
+                    if((bool)$jawaban->correct) {
+                        $jawabanBenar[] = $jawaban->content;
+                    }
+                }
+
+                foreach($doActivitySoal->doActivityJawabans as $doActivityJawaban) {
+                    if(in_array($doActivityJawaban->jawaban, $jawabanBenar)) {
+                        $jumlahBenar++;
+                    }
+                }
+
+                if($jumlahBenar == count($jawabanBenar)) {
+                    $nilai++;
+                }
+            }
+
+            $nilai = ($nilai / $total_soal) * 100;
+
+            $datas[] = [
+                'author' => $doActivity->activity->user->name,
+                'date' => $doActivity->created_at->format('d M Y'),
+                'mapel_kelas_kategori_kelas' => $doActivity->activity->activityMapelKelasKategoriKelas->map(function ($item) {
+                    return [
+                        'mapel' => $item->mapel->kependekan,
+                        'kelas_kategori' => $item->kelasKategori->kependekan,
+                        'kelas' => $item->kelas->bilangan,
+                    ];
+                }),
+                'nilai' => round($nilai, 2)
+            ];
+        }
+
+        return inertia('Auth/Rapor', [
+            'datas' => $datas
+        ]);
     }
 }
--- resources/js/Pages/Auth/Rapor.vue
+++ resources/js/Pages/Auth/Rapor.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
 import AuthLayout from '@/Layouts/AuthLayout.vue';
 import CustomHead from '@/Components/CustomHead.vue';
+
+const props = defineProps<{
+    datas: any;
+}>();
+
 </script>
 
 <template>
     <CustomHead title="Rapor" />
     <AuthLayout title="Rapor">
-        <h1>Ini adalah halaman rapor</h1>
+        <div class="flex flex-row flex-wrap gap-3">
+            <div v-for="(data, index) in props.datas" :key="index" class="flex flex-col gap-3 bg-white rounded-lg p-5 w-full max-w-sm h-fit">
+                <span><strong>Guru :</strong> <i>{{ data.author }}</i></span>
+                <span><strong>Tanggal :</strong> <i>{{ data.date }}</i></span>
+                <div v-for="(mkk, index) in data.mapel_kelas_kategori_kelas" :key="index" class="border border-gray-300 p-3">
+                    <h1>{{ mkk.mapel }}</h1>
+                    <p>{{ mkk.kelas }} - {{ mkk.kelas_kategori }}</p>
+                </div>
+                <p><strong>Nilai :</strong> {{ data.nilai }}</p>
+            </div>
+        </div>
+        
     </AuthLayout>
 </template>
